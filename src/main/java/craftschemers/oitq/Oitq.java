712f5ade68b3e2b3c0878dce394648bef8b830a3
Andrew <andrewvad517@gmail.com>
2022-05-12 14:12:39 -0400

    initial commit

---
+++ src/main/java/craftschemers/oitq/Oitq.java
@@ -0,0 +1,17 @@
+package craftschemers.oitq;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Oitq extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
